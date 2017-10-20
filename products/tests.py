e08290c0f75d765673688a11d6c3e7dab4a76089
Niel <nielmce@gmail.com>
2017-10-20 08:55:30 +0000

    added test to products

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
-# Create your tests here.
+from .models import Product
+ 
+ 
+class ProductTests(TestCase):
+    """
+    Here we'll define the tests
+    that we'll run against our Post model
+    """
+ 
+    def test_str(self):
+        test_name = Product(name='A product')
+        self.assertEqual(str(test_name),
+                          'A product')
